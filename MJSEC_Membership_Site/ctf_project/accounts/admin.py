4989c4e2f5078cd8af20b56b224cc2a811838cad
Jongcoding <ialleejy@gmail.com>
2025-05-07 18:57:36 +0900

    [UPDATE]: accounts admin 관리 추가

--- MJSEC_Membership_Site/ctf_project/accounts/admin.py
+++ MJSEC_Membership_Site/ctf_project/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # 관리자 페이지에 프로필 정보를 표시할 컬럼 설정
+    list_display = (
+        'user',          # 사용자
+        'student_id',    # 학번
+        'department'     # 학과
+    )
+    # 관리자 페이지 검색 기능에 사용할 필드 설정
+    search_fields = (
+        'user__username',
+        'student_id',
+        'department'
+    )
